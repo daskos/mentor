4a67544762b500f0c80420681510d73d7012bcb0
Zoltan Nagy <zoltan.nagy@replise.com>
2016-02-15 08:53:50 +0100

    task resource handling updated, closes #5

--- satyr/event_handlers.py
+++ satyr/event_handlers.py
@@ -12,16 +12,17 @@ def resource_offer_handler(self, driver, offers):
     def get_resources_from_offer(offer):
         return {res.name: res.scalar.value for res in offer.resources}
 
-    def calculate_available_places(resources):
-        divide = lambda name, val: floor(val / self.config['resources'][name])
-        results = [divide(name, val) for name, val in resources.iteritems() if name in self.config['resources']]
-        return min(results)
-
     def add_resource(task, name, value):
         res = task.resources.add()
         res.name = name
         res.type = mesos_pb2.Value.SCALAR
         res.scalar.value = value
+
+    def create_task_resources(data):
+        if isinstance(data, dict) and data.get('resources'):
+            return data['resources']
+
+        return self.config['resources']
 
     def create_task(offer, data):
         self.task_stats['created'] += 1
@@ -33,10 +34,30 @@ def resource_offer_handler(self, driver, offers):
         task.executor.MergeFrom(self.task_executor)
         task.data = data
 
-        for name, val in self.config['resources'].iteritems():
+        for name, val in create_task_resources(data).items():
             add_resource(task, name, val)
 
         return task
+
+    def task_fits_into_remaining_resources(resources, task_resources):
+        return all([val - task_resources.get(name, 0) > 0 for name, val in resources.items() if name in task_resources])
+
+    def create_new_task_resources(resources, task_resources):
+        return {name: val - task_resources.get(name, 0) for name, val in resources.items() if name in task_resources}
+
+    def create_task_list(resources, tasks):
+        if not len(self.task_queue):
+            return tasks
+
+        task = self.task_queue.popleft()
+        task_resources = create_task_resources(task)
+        if task_fits_into_remaining_resources(resources, task_resources) and len(tasks) < self.config['max_tasks']:
+            tasks.append(task)
+            return create_task_list(create_new_task_resources(resources, task_resources), tasks)
+
+        self.task_queue.appendleft(task)
+
+        return tasks
 
     def handle_offers():
         self.shutdown_if_done(driver)
@@ -46,13 +67,7 @@ def resource_offer_handler(self, driver, offers):
                 driver.declineOffer(offer.id, fltr)
                 continue
 
-            resources = get_resources_from_offer(offer)
-            places = min(self.config['max_tasks'] - self.task_stats['running'], calculate_available_places(resources))
-
-            tasks = []
-            while self.task_queue and places > 0:
-                tasks.append(create_task(offer, self.task_queue.popleft()))
-                places -= 1
+            tasks = [create_task(offer, task) for task in create_task_list(get_resources_from_offer(offer), [])]
 
             print 'We\'re starting %d new task(s)' % len(tasks)
             driver.launchTasks(offer.id, tasks) if tasks else driver.declineOffer(offer.id, fltr)

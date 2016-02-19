cca5ff569bb6065aad4f9467bfa621a6ece8cdaf
Zoltan Nagy <zoltan.nagy@replise.com>
2016-02-19 09:43:31 +0100

    config object

--- satyr/config.py
+++ satyr/config.py
@@ -1,5 +1,6 @@
 import os
 from .executors import pickled as pickled_executor
+from copy import copy
 
 default = {
     'id': 'satyr',
@@ -10,7 +11,20 @@ default = {
     'user': 'root',
     'executor_dir': os.path.dirname(pickled_executor.__file__),
     'executor_file': 'pickled.py',
-    'command': 'python pickled.py',
+    'command': 'python -m satyr.executors.pickled',
     'permanent': False,
     'filter_refuse_seconds': 300
 }
+
+
+class Config(dict):
+    def __init__(self, conf={}):
+        """Reading config, always overwriting any value w/ the ones
+        set in environment variables started as SATYR_."""
+        d = copy(default)
+        d.update(conf)
+        for k, v in d.items():
+            setattr(self, k, os.getenv('SATYR_' + k.upper(), v))
+
+    def __getitem__(self, key):
+        return getattr(self, key)

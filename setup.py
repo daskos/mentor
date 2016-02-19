1d8c2b540f13f5a5568a5ede91709e5a3c32b7d9
Zoltan Nagy <zoltan.nagy@replise.com>
2016-02-19 09:43:11 +0100

    setup.py updated w/ subpackages

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='satyr',
       maintainer_email='zoltan.nagy@lensa.com',
       license='BSD',
       keywords='mesos framework multiprocessing',
-      packages=['satyr'],
+      packages=['satyr', 'satyr.executors', 'satyr.event_handlers'],
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       install_requires=['cloudpickle'],

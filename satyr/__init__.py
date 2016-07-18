f01d7f4463b6a5fe971d75ad09c5cf4d766551c6
Szucs Krisztian <szucs.krisztian@gmail.com>
2016-07-18 11:16:37 +0200

    Removed logging configuration from __init__

--- satyr/__init__.py
+++ satyr/__init__.py
@@ -1,13 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
-import logging
 import pkg_resources as _pkg_resources
 
 from .scheduler import QueueScheduler
 from .executor import OneOffExecutor
 from .messages import PythonTask, PythonTaskStatus  # important to register classes
 
-logging.basicConfig(format='%(relativeCreated)6d %(threadName)s %(message)s')
 
 __version__ = _pkg_resources.get_distribution('satyr').version
 
